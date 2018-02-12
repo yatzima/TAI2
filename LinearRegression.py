80f7e040bcd03e1efb2c7a372566e00b39c6e4c9
Justin Ma <justinmazk@gmail.com>
2018-02-12 10:29:37 +0100

    Merge conflict

--- LinearRegression.py
+++ LinearRegression.py
@@ -5,17 +5,22 @@ x1, y1 = np.loadtxt('salammbo_a_en', delimiter=',', unpack=True)
 x2, y2 = np.loadtxt('salammbo_a_fr', delimiter=',', unpack=True)
 
 plt.figure(1)
-plt.plot(x1,y1, label='Loaded from file!')
+plt.plot(x1,y1,'ro', label='English')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Interesting Graph\nCheck it out')
+plt.title('English')
 plt.legend()
 plt.show()
 
 plt.figure(2)
-plt.plot(x2,y2, label='Loaded from file!')
+plt.plot(x2,y2,'ro', label='French')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Interesting Graph\nCheck it out 2')
+plt.title('French')
 plt.legend()
 plt.show()
+
+
+#For a data set ds, we find the minimum through a walk (iteration) down the surface.
+def gradDes(ds):
+    
