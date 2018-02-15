374bfe3a806cf9070bc69ab0d5e78c86ba5c39c9
Justin Ma <justinmazk@gmail.com>
2018-02-15 11:52:01 +0100

    Reader is not solved

--- ThePerceptron.py
+++ ThePerceptron.py
@@ -1,22 +1,27 @@
 import numpy as np
-import matlabplot.plot
+from sklearn import *
+from svmutil import *
 
 x1, y1 = np.loadtxt('salammbo_a_en', delimiter=',', unpack=True)
 x2, y2 = np.loadtxt('salammbo_a_fr', delimiter=',', unpack=True)
+
+alpha = 0.1
+
+# Write a reader function for the LIBSVM format and scale the data in your set. You can write a simplified
+# reader that assumes that all the attributes, including zeros, will have an index, i.e. ignore the sparse format.
+def LIBSVMreader():
+    file = open('salammbo_a', 'r')
+    #print(file.read())
+    for line in file:
+        words = line.split(':')
+        #print(words)
+    #print(words)
 
 # Scales the data to [0, 1]
 x1 = (x1 - np.min(x1))/(np.max(x1) - np.min(x1))
 y1 = (y1 - np.min(y1))/(np.max(y1) - np.min(y1))
 x2 = (x2 - np.min(x2))/(np.max(x2) - np.min(x2))
 y2 = (y2 - np.min(y2))/(np.max(y2) - np.min(y2))
-
-alpha = 0.1
-
-# Write a reader function for the LIBSVM format and scale the data in your set. You can write a simplified
-# reader that assumes that all the attributes, including zeros, will have an index, i.e. ignore the sparse format.
-
-#def LIBSVMreader(ds):
-
 
 # Write the perceptron program as explained in pages 723--725 in Russell-Norvig
 # and in the slides and run it on your data set.
@@ -33,3 +38,8 @@ def updateWeight(x, y, w):
 # Define the perceptron
 def perceptron(x, y, w):
     return 1
+
+LIBSVMreader()
+svm_read_problem('salammbo_a')
+print(prob_y)
+print(prob_x)
