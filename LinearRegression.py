6c3014e040ce98db4aa99972b331ab77edbb0caa
fridab <frida.bornfors@gmail.com>
2018-02-16 16:16:01 +0100

    Nicer plots, tunes alpha and epsilon

--- LinearRegression.py
+++ LinearRegression.py
@@ -13,8 +13,8 @@ x2_scaled = (x2 - np.min(x2)) / (np.max(x2) - np.min(x2))
 y2_scaled = (y2 - np.min(y2)) / (np.max(y2) - np.min(y2))
 
 # Constants
-alpha = 1
-epsilon = 0.01
+alpha = 0.1
+epsilon = 0.001
 
 # Add column of ones to X array
 a = np.ones(np.size(x1_scaled))
@@ -105,8 +105,8 @@ def dSSE(x, y, w):
 
 w_e1 = batchGradDes(x1_scaled, y1_scaled, alpha)  # English regression
 w_f1 = batchGradDes(x2_scaled, y2_scaled, alpha)  # French regression
-w_e2 = stochGradDes(x1_scaled, y1_scaled, 0.1)  # English regression
-w_f2 = stochGradDes(x2_scaled, y2_scaled, 0.1)  # French regression
+w_e2 = stochGradDes(x1_scaled, y1_scaled, alpha)  # English regression
+w_f2 = stochGradDes(x2_scaled, y2_scaled, alpha)  # French regression
 
 # Print out coefficients
 print(w_e1)
@@ -120,9 +120,11 @@ y_f2 = w_f2[0] + w_f2[1] * xreg
 y_e1 = w_e1[0] + w_e1[1] * xreg
 y_e2 = w_e2[0] + w_e2[1] * xreg
 
+plt.close("all")
+
 plt.figure(1)
 plt.plot(xreg, y_e1, label='English - batch')
-plt.plot((x1_scaled[:, 1]), y1_scaled, 'ro')
+plt.plot((x1_scaled[:, 1]), y1_scaled, 'ro', label='Scaled points from data set')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('English')
@@ -131,7 +133,7 @@ plt.show()
 
 plt.figure(2)
 plt.plot(xreg, y_f1, label='French - batch')
-plt.plot((x2_scaled[:, 1]), y2_scaled, 'ro')
+plt.plot((x2_scaled[:, 1]), y2_scaled, 'ro', label='Scaled points from data set')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('French')
@@ -140,7 +142,7 @@ plt.show()
 
 plt.figure(3)
 plt.plot(xreg, y_e2, label='English - stochastic')
-plt.plot((x1_scaled[:, 1]), y1_scaled, 'ro')
+plt.plot((x1_scaled[:, 1]), y1_scaled, 'ro', label='Scaled points from data set')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('English')
@@ -149,7 +151,7 @@ plt.show()
 
 plt.figure(4)
 plt.plot(xreg, y_f2, label='French - stochastic')
-plt.plot((x2_scaled[:, 1]), y2_scaled, 'ro')
+plt.plot((x2_scaled[:, 1]), y2_scaled, 'ro', label='Scaled points from data set')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('French')
@@ -174,3 +176,45 @@ print(ke_stoch)
 print(me_stoch)
 print(kf_stoch)
 print(mf_stoch)
+
+xreg= np.linspace(0, 80000, 50, True)
+y_f1 = mf_batch + kf_batch * xreg
+y_f2 = mf_stoch + kf_stoch * xreg
+y_e1 = me_batch + ke_batch * xreg
+y_e2 = me_stoch + ke_stoch * xreg
+
+plt.figure(5)
+plt.plot(xreg, y_e1, label=("$y= %.3f + %.5f * x$" %(me_batch,ke_batch)))
+plt.plot(x1, y1, 'ro', label='Points from data set')
+plt.xlabel('Number of characters')
+plt.ylabel('Number of A')
+plt.title('English - batch gradient descent')
+plt.legend()
+plt.show()
+
+plt.figure(6)
+plt.plot(xreg, y_f1, label=("$y= %.3f + %.5f * x$" %(mf_batch,kf_batch)))
+plt.plot(x2, y2, 'ro', label='Points from data set')
+plt.xlabel('Number of characters')
+plt.ylabel('Number of A')
+plt.title('French - batch gradient descent')
+plt.legend()
+plt.show()
+
+plt.figure(7)
+plt.plot(xreg, y_e2, label=("$y= %.3f + %.5f * x$" %(me_stoch,ke_stoch)))
+plt.plot(x1, y1, 'ro', label='Points from data set')
+plt.xlabel('Number of characters')
+plt.ylabel('Number of A')
+plt.title('English - Stochastic gradient descent')
+plt.legend()
+plt.show()
+
+plt.figure(8)
+plt.plot(xreg, y_f2, label=("$y= %.3f + %.5f * x$" %(mf_stoch,kf_stoch)))
+plt.plot(x2, y2, 'ro', label='Points from data set')
+plt.xlabel('Number of characters')
+plt.ylabel('Number of A')
+plt.title('French - Stochastic gradient descent')
+plt.legend()
+plt.show()
